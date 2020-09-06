7dcfb570c73e183a2a3ecb8f385d2ad5df87cac1
tahirbhalli <tahirbhalli16@gmail.com>
2020-09-06 22:48:44 +0500

    Movie tests

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'add movie without name' do
+    a = Movie.new(description: 'demo description', director: 'abv')
+    a.save
+    expect(Movie.exists?(director: 'abv')).to eq(false)
+  end
+  it 'add movie with name' do
+    a = Movie.new(name: 'demo name', director: 'abv')
+    a.save
+    expect(Movie.exists?(director: 'abv')).to eq(true)
+  end
 end
