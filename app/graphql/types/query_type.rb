2a6b25823bb80de2b1101e3a3c110023a45942ae
tahirbhalli <tahirbhalli16@gmail.com>
2020-09-07 20:10:33 +0500

    graphql required quries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,29 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
 
-    # TODO: remove me
     field :user, Types::UserType, null: false, description: 'get the user by id'
-    field :movies, [Types::MovieType], null: false, description: 'get all the movies'
-    # Ex:- :null => false
-    def user()
+    field :allmovies, [Types::MovieType], null: false, description: 'get all the movies'
+    field :movie, Types::MovieType,null: false,description: 'get specfic Movie' do
+      argument :id, Integer,required: true 
+    end
+    field :allstars,[Types::StarType],null: false,description: 'get all the list of stars'
+    field :star,Types::StarType,null: false,description: 'get specfic star' do
+      argument :id, Integer,required: true 
+    end
+    def movie(id:)
+      Movie.find(id)
+    end
+    def star(id:)
+      Star.find(id)
+    end
+    def user
       User.find(1)
     end
-    def movies
+    def allmovies
       Movie.all
+    end
+    def allstars
+      Star.all
     end
   end
 end
---
+++ app/graphql/types/star_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class StarType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :pic_url, String, null: true
+    field :description, String, null: true
+
+  end
+end
